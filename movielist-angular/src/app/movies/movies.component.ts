31a48280ef0f3063d618599f5c42435cf428edf7
Shazil Jamal <shaziljamal7003@gmail.com>
2022-06-06 19:21:53 -0700

    Movie Editor Commit

---
+++ movielist-angular/src/app/movies/movies.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../movie';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+
+  movie: Movie = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
