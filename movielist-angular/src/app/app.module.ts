e3b21c02235a0ec782a4c39bbafd74f7f37780e6
Shazil Jamal <shaziljamal7003@gmail.com>
2022-06-08 13:22:02 -0700

    Adding Dashboard

--- movielist-angular/src/app/app.module.ts
+++ movielist-angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     MovieDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
